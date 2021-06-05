f0a30b4bb8632c46818903574e7d8a351de23fc8
Dahak Chiraz <dahakchiraz11@gmail.com>
2021-06-05 01:06:21 -1200

    initial commit

---
+++ database/migrations/2021_06_05_100852_create_employe_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employe', function (Blueprint $table) {
+            $table->id('matricule');
+            $table->string('nom_emp');
+            $table->string('prenom_emp');
+            $table->date('date_recrute');
+            $table->string('situato_fami');
+            $table->boolean('etat_salaire');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migratioS
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employe');
+    }
+}
